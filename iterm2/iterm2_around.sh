e846bf70d8d5a1bffcb21edc9be24df8f9ec25a1
观小过 <kuanhsiaokuo@guanxiaoguos-MacBook-Pro.local>
2022-04-12 12:15:18 +0800

    将zshrc配置文件切换到分支下，方便随时管理

--- iterm2/iterm2_around.sh
+++ iterm2/iterm2_around.sh
@@ -4,11 +4,11 @@ brew install --cask font-hack-nerd-font
 brew install lsd
 pip3 install tldr
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp -r zsh_config ~/
-echo "source ~/zsh_config/custom_config" >>~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "source ${PWD}/zsh_config/custom_config" >> ~/.zshrc
 source ~/.zshrc
+p10k configure
