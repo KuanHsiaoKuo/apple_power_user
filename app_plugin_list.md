84039f4b7657c3abf126a70938a8fc19699e57aa
KuanHsiaoKuo <kuanhsiaokuo@gmail.com>
2022-05-17 18:54:37 +0800

    添加自动生成toc的脚本

--- app_plugin_list.md
+++ app_plugin_list.md
@@ -241,7 +241,7 @@ sudo cpan Graph:Easy
 > [Help – Hook](https://hookproductivity.com/help)
 
 - hook作者还是链接宣言的发起者： [让链接无处不在——《泛在链接宣言》的理论与理想 - 少数派](https://web.archive.org/web/20220516030303/https://sspai.com/post/70648)
-
+![](https://web.archive.org/web/20220516030303im_/https://cdn.sspai.com/article/4513147f-c57b-d9aa-e37b-69aecfbd91ca.png?imageMogr2/auto-orient/quality/95/thumbnail/!1420x708r/gravity/Center/crop/1420x708/interlace/1)
 #### [Zotero | Your personal research assistant](https://www.zotero.org/)
 
 > Zotero is a free, easy-to-use tool to help you collect, organize, cite, and share research.
---
+++ scripts/toc_generator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# It will read the input and:
+# [One liner to generate a Markdown TOC | by Andres Rodriguez | Medium](https://medium.com/@acrodriguez/one-liner-to-generate-a-markdown-toc-f5292112fd14)
+# 1. Extract only the headers via grep (using $1 and $2 as first and last heading level)
+# 2. Extract the header text via sed and created ':' separated records of the form '###:Full Text:Full Text'
+# 3. Compose each TOC line via awk by replacing '#' with '  ' and stripping spaces and caps of reference
+# TEST:
+#   sudo chmod 777 toc_generator.sh
+#   curl -s https://raw.githubusercontent.com/felixge/node-mysql/master/Readme.md | ./toc_generator.sh 2 2
+
+grep -E "^#{${1:-1},${2:-2}} " | \
+sed -E 's/(#+) (.+)/\1:\2:\2/g' | \
+awk -F ":" '{ gsub(/#/,"  ",$1); gsub(/[ ]/,"-",$3); print $1 "- [" $2 "](#" tolower($3) ")" }'
