de5d4166f4b7c7f5ea7e609321ada8fe591fc925
KuanHsiaoKuo <16441042+KuanHsiaoKuo@users.noreply.github.com>
2022-06-05 08:16:49 +0000

    Deploying to gh-pages from @ KuanHsiaoKuo/osx_basic_configuration@e54a8d698e10ca1617c0db6baec847a3fcc7bebd 🚀

---
+++ app_extensions/iterm2_around/iterm2_around.sh
@@ -0,0 +1,14 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+brew install lsd
+pip3 install tldr
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "source ${PWD}/zsh_config/custom_config" >> ~/.zshrc
+source ~/.zshrc
+p10k configure
