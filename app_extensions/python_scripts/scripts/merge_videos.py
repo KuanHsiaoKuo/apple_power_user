b6d75f57ae64624a3824b5875ba83eb37843389f
KuanHsiaoKuo <16441042+KuanHsiaoKuo@users.noreply.github.com>
2022-09-04 16:04:46 +0000

    Deploying to gh-pages from @ KuanHsiaoKuo/apple_power_user@fd9741708176a1235df4542674f3a8c5e2db014d 🚀

--- app_extensions/python_scripts/scripts/merge_videos.py
+++ app_extensions/python_scripts/scripts/merge_videos.py
@@ -5,13 +5,6 @@ import re
 # brew install ffmpeg
 
 
-def list_order(file_list):
-    pattern = re.compile(r'([0-9]+)')
-    for item in file_list:
-        print(pattern.findall(item)[0])
-    file_list.sort(key=lambda x: int(pattern.findall(x)[0]))
-
-
 def supported_merge(file_list, target_dir):
     """
     ffmpeg只支持默认格式合并:
@@ -25,6 +18,7 @@ def supported_merge(file_list, target_dir):
     target_file = "output"
     status = os.system(merge_cmd % (files_arg, target_dir, target_file))
     os.system(f"ffmpeg -i {target_dir}/{target_file}.mpg -y -qscale 0 -vcodec libx264 {target_dir}/{target_file}.mp4")
+    # os.system(f"ffmpeg -i {target_file}.mpg {target_file}.MP4")
     print(status)
 
 
@@ -32,14 +26,14 @@ def mp4_to_mpg(file_list):
     for index, mp4 in enumerate(file_list):
         file_name = mp4.split('.')[0]
         status = os.system(f'ffmpeg -i {mp4} -qscale 4 {file_name}.mpg')
-        print(f"{index+1}/{len(file_list)}: {status} {file_name}.mpg")
+        print(f"{index + 1}/{len(file_list)}: {status} {file_name}.mpg")
 
 
 if __name__ == "__main__":
-    target_dir = ''
-    cmd_res = os.popen(f'ls {target_dir}').read()
+    target_dir = '/Users/kuanhsiaokuo/Migrations/videos/张汉东的Rust实战课/Rust异步编程之tokio运行时'
+    cmd_res = os.popen(f'ls {target_dir}/*.mp4').read()
     file_list = [item for item in cmd_res.split('\n') if item]
-    list_order(file_list)
-    file_list = [f"{target_dir}/{file}" for file in file_list]
-    # mp4_to_mpg(file_list)
+    pattern = re.compile(r'([0-9]+)')
+    file_list.sort(key=lambda x: int(pattern.findall(x)[0]))
+    mp4_to_mpg(file_list)
     supported_merge(file_list, target_dir)
